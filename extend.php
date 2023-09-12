8a9ac1233bc3574b155e9254f349c3515bf4ad8d
BetterFlarum Vietnam <143252203+betterflarum@users.noreply.github.com>
2023-09-12 07:26:10 +0700

    Add files via upload

---
+++ extend.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of avokadogames/auth-discord.
+ *
+ * Copyright (c) 2019 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace AvokadoGames\AuthDiscord;
+
+use Flarum\Extend;
+use AvokadoGames\AuthDiscord\Controllers\DiscordAuthController;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/resources/less/forum.less'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js'),
+    new Extend\Locales(__DIR__.'/resources/locale'),
+    (new Extend\Routes('forum'))
+        ->get('/auth/buildcord', 'auth.discord', DiscordAuthController::class),
+];
